d79ad3daaac151c9e394fd6f854938053942872a
SpaceCadetVulture <Vito.Santiago@protonmail.com>
2022-12-25 23:30:04 +0100

    2.4 Task
    Task 2.4

--- index.js
+++ index.js
@@ -1,11 +1,59 @@
 const express = require('express');
 const app = express();
 
-const http = require('http');
+let topMovies = [
+    {
+      title: 'Shawshank Redemption',
+    },
+    {
+      title: 'The Godfather',
+    },
+    {
+      title: 'The Dark Knight',
+    },
+    {
+        title: 'The Godfather 2',
+      },
+      {
+        title: '12 Monkeys',
+      },
+      {
+        title: 'Shrek',
+      },
+      {
+        title: 'Lord of the Rings',
+      },
+      {
+        title: 'Pulp Fiction',
+      },
+       {
+      title: 'Fight Club',
+    },
+    {
+        title: 'Zombie',
+      },
+  ];
 
-http.createServer((request, response) => {
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end('Welcome to my book club!\n');
-}).listen(8080);
+  // GET requests
+app.get('/', (req, res) => {
+    res.send('Top Ten Movies');
+  });
+  app.get('/documentation', (req, res) => {                  
+    res.sendFile('public/documentation.html', { root: __dirname });
+  });
 
-console.log('My first Node test server is running on Port 8080.');
+  app.get('/movies', (req, res) => {
+    res.json(topMovies);
+  });
+
+  // listen for requests
+app.listen(8080, () => {
+    console.log('Your app is listening on port 8080.');
+  });
+
+app.use(express.static('public'));
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+  });
