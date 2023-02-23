ec64126356622577cd92815cf922536c4181526e
SpaceCadetVulture <Vito.Santiago@protonmail.com>
2023-02-23 22:52:23 +0100

    2.9 edit
    added the authorization, stuck at /users

--- index.js
+++ index.js
@@ -11,6 +11,9 @@ const morgan = require('morgan');
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+let auth = require('./auth')(app);
+const passport = require('passport');
+require('./passport');
 const fs = require('fs');
 const path = require('path');
 
@@ -280,15 +283,16 @@ app.delete('/users/:Username', (req, res) => {
   });
 
 //get all movies
-app.get('/movies', (req, res) => {
+
+app.get('/movies', passport.authenticate('jwt', { session: false }), (req, res) => {
   Movies.find()
-  .then((movies) => {
-  res.status(200).json(movies);
-  })
-  .catch((err) => {
-  console.error(err);
-  res.status(500).send('Error: ' + err);
-  });
+    .then((movies) => {
+      res.status(201).json(movies);
+      })
+      .catch((error) => {
+        console.error(error);
+        res.status(500).send('Error: ' + error);
+      });
   });
 
 // Get all users
